00d358f4e1792e53136373ff601c25f861396c36
GonzalezMelany <mela.glez9@gmail.com>
2026-02-13 15:28:43 -0800

    while loop con python

--- sesion_03.py
+++ sesion_03.py
@@ -13,6 +13,18 @@ for i in range(2,9):
             print(i)
 
 mi_lista_2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+
 for i in mi_lista_2:
        if i != "Lunes":
               print(f"Feliz{i}!")
+
+#while loop
+i=0
+
+while i < 5:
+       i +=1
+       if i==3:
+              continue
+       print(i)
+else:
+       print("i es ahora mayor o igual a 5")
