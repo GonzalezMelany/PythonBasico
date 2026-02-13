f9617bc8f6b7d7e03a45c76c233cba460cc3522b
Your Name <mela.glez9>
2026-02-12 18:37:26 -0800

    Par o impar

---
+++ sesion_02.py
@@ -0,0 +1,9 @@
+#Tuplas
+mi_tupla = (2, 4)
+print ("Mi tupla: ", mi_tupla)
+
+#Lista
+mi_lista = [1, 3.1416, "Melany", mi_tupla]
+print ("El primer elemento de mi lista", mi_lista[0])
+print ("El cuartp elemento de mi lista", mi_lista[3])
+print ("El tercer elemento de mi lista", mi_lista[2])
