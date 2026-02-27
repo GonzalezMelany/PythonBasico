c08e51f7a822895708eed1cb8f683bef8e1fb1c2
GonzalezMelany <mela.glez9@gmail.com>
2026-02-27 15:52:07 -0800

    m

--- OOP/Ogro.py
+++ OOP/Ogro.py
@@ -1,4 +1,5 @@
 from Enemigo import *
+import random
 
 class ogro(Enemigo):
     def __init__(self, punto_energia=20, ataque=3):
@@ -6,3 +7,10 @@ class ogro(Enemigo):
 
     def habla(self):
         print("Ogro aplasta todo!!!")
+
+    def ataque_especial(self):
+        print ("Ogro ataque especial")
+        funciona_ataque_especial = random.random() <0.20
+        if funciona_ataque_especial:
+            self.ataque +=4
+            print('Ogro enojado y incremento su ataque por 4!!!!')
